9a413929959e2b85f1da0b37a6cccf2db9fc11a2
Maharshi Das <maharshidas001@gmail.com>
2025-08-21 17:56:21 +0530

    Fix server

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const port = envConfig.port || 3000;
 const startServer = async () => {
   connectDB()
     .then(() => {
-      app.listen(port, () => {
+      app.listen(port as number, "0.0.0.0", () => {
         console.log(`Server started on http://localhost:${port}\n`);
       });
     });
