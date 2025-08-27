c3ae40943368df37e0abb898f3e1df0742bb0852
Maharshi Das <maharshidas001@gmail.com>
2025-08-27 19:15:23 +0530

    [fix]: Cookie reading error

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -201,12 +201,14 @@ const renewAccessToken = asyncHandler(async (
   const accessOption = {
     httpOnly: true,
     secure: true,
+    sameSite: "none" as const,
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1d
   };
 
   const refreshOption = {
     httpOnly: true,
     secure: true,
+    sameSite: "none" as const,
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
   };
 
